4325fcd916e19af021ab737a53b58c04f8071453
Sagar Khadka <khadkasagar109@gmail.com>
2023-09-26 23:51:03 +0545

    middleware for private routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,12 @@ const {
   registerUser,
   currentUser
 } = require('../controllers/loginController')
+const validateToken = require('../middleware/validateTokenHandler')
 
 const router = express.Router()
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
-router.route('/current').get(currentUser)
+router.route('/current').get(validateToken, currentUser)
 
 module.exports = router
